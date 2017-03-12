68687b84eb9a9470911d65a8f6c380850826c061
dhruv3 <dhruv.mnit@gmail.com>
2017-03-11 20:29:07 -0500

    Initial commit

---
+++ htmlVolumeCalc/script.js
@@ -0,0 +1,22 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Dhruv
+ * Date: 3/10/17
+ * Time: 9:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function(){
+
+    $("input[type='radio']").on("change", function(){
+        chkPanelChanged(this);
+    });
+});
+
+function chkPanelChanged(self){
+    var $radioGrp = $("form :radio");
+    var
+    if($radioGrp[1].checked == true){
+
+    }
+}

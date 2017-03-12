316e9626f06209faaeef8a174cec66828228884d
dhruv3 <dhruv.mnit@gmail.com>
2017-03-11 21:42:47 -0500

    Working except styling

--- htmlVolumeCalc/script.js
+++ htmlVolumeCalc/script.js
@@ -9,14 +9,76 @@
 $(document).ready(function(){
 
     $("input[type='radio']").on("change", function(){
-        chkPanelChanged(this);
+        chkPanelChanged();
+    });
+
+    $("select").on("change", function(){
+        shapeSelected(this.value);
+    });
+
+    $("#result").on("click", function(){
+        calculateVol();
+    });
+
+    $("#reset").on("click", function(){
+        resetForm();
     });
 });
 
-function chkPanelChanged(self){
-    var $radioGrp = $("form :radio");
-    var
-    if($radioGrp[1].checked == true){
+function resetForm(){
+    $("#radiusIPBox").val("");
+    $("#heightIPBox").val("");
+}
 
+function calculateVol(){
+    var shape = $("select").val();
+    var rad = $("#radiusIPBox").val();
+    var height = $("#heightIPBox").val();
+    var vol = 0;
+    switch(shape){
+        case "Cone":
+            vol = (Math.PI * (rad^2) * height) / 3;
+            break;
+        case "Cylinder":
+            vol = Math.PI * (rad^2) * height;
+            break;
+        case "Sphere":
+            vol =   (4*Math.PI*(rad^3))/ 3;
+            break;
+    }
+    if(isNaN(vol))
+        alert("Incorrect input params");
+    else{
+        $("#volVal").text(vol);
+        $("#radiusVal").text(rad);
+        $("#shapeVal").text(shape);
+        if(shape != "Sphere")
+            $("#heightVal").text(height);
     }
 }
+
+function chkPanelChanged(){
+    var $radioGrp = $("form :radio");
+    if($radioGrp[0].checked == true){
+        $("#unit").text("English");
+        $("#unit1").text("ft");
+        $("#unit2").text("ft");
+        $("#unit3").text("ft");
+    }
+    else{
+        $("#unit").text("SI");
+        $("#unit1").text("m");
+        $("#unit2").text("m");
+        $("#unit3").text("m");
+    }
+}
+
+function shapeSelected(shape){
+    $("#shape").text(shape);
+    $("#shapeVal").text(shape);
+    if(shape == "Sphere")
+        $("#heightIPBox").prop("disabled", true);
+    else
+        $("#heightIPBox").prop("disabled", false);
+}
+

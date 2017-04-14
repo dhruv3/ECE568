36e76822a5a2d4a2855cab7be723fd675e722a66
dhruv3 <dhruv.mnit@gmail.com>
2017-04-14 01:02:32 -0400

    Adding new files and updating functionalities

--- src/networkProgramming/ClientSide.java
+++ src/networkProgramming/ClientSide.java
@@ -5,45 +5,58 @@ import java.net.*;
 
 public class ClientSide {
 	public static void main(String[] args) {
+		//accept two arguments, the server host name and its port number
 		if (args.length != 2) { // Test for correct num. of arguments
 			 System.err.println("ERROR server host name AND port number not given");
 			 System.exit(1);
 		}
+		//convert the port number, input as a string, to an integer number
 		int port_num = Integer.parseInt(args[1]);
 		while(true) {
 			try {
+				//create a character-oriented input socket stream to read server’s responses
 				Socket c_sock = new Socket(args[0], port_num);
 				BufferedReader in = new BufferedReader(new InputStreamReader(c_sock.getInputStream()));
+				//create a character-oriented output socket stream to write request messages
 				PrintWriter out = new PrintWriter(
 						new OutputStreamWriter(c_sock.getOutputStream()),true);
+				//create a character-oriented input stream to read user’s keyboard input from the standard input stream System.in
 				BufferedReader userEntry = new BufferedReader(new InputStreamReader(System.in));
 				System.out.print("User, enter your message: ");
-				String command = userEntry.readLine();
-				String[] cmds = command.split(" ");
-				out.println(command);
+				String clientInp = userEntry.readLine();
+				String[] clientCmd = clientInp.split(" ");
+				//sends request message to the server by calling println() on the output stream
+				out.println(clientInp);
 				System.out.println("Server says: ");
 				String str = in.readLine();
 				while(str != null) {
 					System.out.println(str);
 					str = in.readLine();
 				}
-				if(cmds[0].equals("EXIT")) {
-					if(cmds.length == 1) {
+				
+				if(clientCmd[0].equals("EXIT")) {
+					if(clientCmd.length == 1) {
 						System.out.println("Normal_Exit");
 					} else {
-						System.out.println("EXIT CODE: " + cmds[1]);
+						System.out.print("EXIT CODE: ");
+						for(int i = 1; i < clientCmd.length; i++) {
+							System.out.print(clientCmd[i] + " ");
+						}
+						System.out.print("\n");
 					}
 					c_sock.close();
 					break;
 				}
-				//if(!c_sock.isClosed()) {
+				//closes the connection after a single exchange of messages
+				if(!c_sock.isClosed()) {
 					c_sock.close();
-				//}
+				}
 			} 
 			catch (IOException ex) { 
 				ex.printStackTrace();
 			} 
 		}
+		//client program dies
 		System.exit(0);
 	} 
 }
--- src/networkProgramming/ServerSide.java
+++ src/networkProgramming/ServerSide.java
@@ -5,9 +5,9 @@ import java.net.*;
 
 
 public class ServerSide {
-	
+	//function to read and then print contents of a file
 	private static void readFile(String fileName, PrintWriter out) {
-		File file = new File(fileName);
+		File file = new File("src/networkProgramming/" + fileName);
 		BufferedReader reader = null;
 		String line = null;
 		try {
@@ -24,11 +24,14 @@ public class ServerSide {
 	}
 
 	public static void main(String[] args) {
+		//define the server class with only one method, main(). The program accepts a single argument i.e. the server’s port number
 		if (args.length != 1) { // Test for correct num. of arguments
 			 System.err.println( "ERROR server port number not given");
 			 System.exit(1);
 		}
+		//convert the port number, input as a string, to an integer number
 		int port_num = Integer.parseInt(args[0]);
+		//create the well-known server socket
 		ServerSocket rv_sock = null;
 		try {
 			rv_sock = new ServerSocket(port_num);
@@ -36,18 +39,21 @@ public class ServerSide {
 		catch (IOException ex) { 
 			ex.printStackTrace(); 
 		}
+		
 		while (true) { // run forever, waiting for clients to connect
 			System.out.println("\nWaiting for client to connect...");
 			try {
+				//the server blocks and waits indefinitely until a client makes connection at which time a Socket object is returned
 				Socket s_sock = rv_sock.accept();
+				//set up the input stream for reading client’s requests
 				BufferedReader in = new BufferedReader(new InputStreamReader(s_sock.getInputStream()));
 				PrintWriter out = new PrintWriter(
 						new OutputStreamWriter(s_sock.getOutputStream()),true);
-				
+				//receive the client’s message by calling readLine() on the input stream
 				String clientInp = in.readLine();
 				String[] clientCmd = clientInp.split(" ");
 				System.out.println("Client's message: " + clientInp); 
-				
+				//take actions based on command given by client
 				switch(clientCmd[0]){
 					case "GET":
 						String fileName = clientCmd[1];
@@ -64,7 +70,8 @@ public class ServerSide {
 					default:
 						out.println("ERROR: no such command");
 				}
-				out.println("I got your message");
+				//out.println("I got your message");
+				//closes the connection after a single exchange of messages
 				s_sock.close();
 			} 
 			catch (IOException ex) { 
